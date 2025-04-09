9d0cc688b1758bc34d61b918c51f1299c96d6f46
TimZer0 <muzalevskaya1973@gmail.com>
2025-04-09 22:59:00 +0800

    four

---
+++ Lab_4-master/Lab1_Graph/Lab1_Graph.cpp
@@ -0,0 +1,184 @@
+﻿#define GLEW_DLL
+#define GLFW_DLL
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "shader_lib.h"
+#include <cstdio>
+#include <cmath>
+#include "glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+glm::vec3 cameraPos = glm::vec3(0.0, 0.0, 5.0);
+glm::vec3 cameraFront = glm::vec3(0.0, 0.0, -1.0);
+glm::vec3 cameraUp = glm::vec3(0.0, 1.0, 0.0);
+float yaw = -90.0f;
+float pitch = 0.0f;
+float lastX = 512.0f / 2.0;
+float lastY = 512.0f / 2.0;
+bool firstMouse = true;
+float fov = 45.0f;
+
+GLfloat points[] = {
+    0.0f, 0.0f, 0.0f,
+    0.5f, 0.0f, 0.0f,
+    0.25f, 0.433f, 0.0f,
+    -0.25f, 0.433f, 0.0f,
+    -0.5f, 0.0f, 0.0f,
+    -0.25f, -0.433f, 0.0f,
+    0.25f, -0.433f, 0.0f
+};
+
+GLuint index[] = {
+    0, 1, 2,
+    0, 2, 3,
+    0, 3, 4,
+    0, 4, 5,
+    0, 5, 6,
+    0, 6, 1
+};
+
+void settingMat4(int ID, const std::string& name, const glm::mat4& mat)
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    fov -= (float)yoffset;
+    if (fov < 1.0f)
+        fov = 1.0f;
+    if (fov > 45.0f)
+        fov = 45.0f;
+}
+
+void processInput(GLFWwindow* window)
+{
+    const float cameraSpeed = 0.001f;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+int main() {
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3.\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(512, 512, "Hexagon", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "ERROR: could not open window with GLFW3.\n");
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    const unsigned int SCR_WIDTH = 1024;
+    const unsigned int SCR_HEIGHT = 768;
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glm::mat4 projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    ShaderProgram shaderProgram("vertex_shader.glsl", "fragment_shader.glsl");
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(1.0, 1.0, 0.3, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        float timeValue = glfwGetTime();
+        float Red = abs(sin(timeValue));
+        float Green = abs(sin(timeValue * 2.0f));
+        float Blue = abs(sin(timeValue * 1.5f));
+
+        projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        shaderProgram.use();
+        settingMat4(shaderProgram.programID, "projection", projection);
+        settingMat4(shaderProgram.programID, "view", view);
+        shaderProgram.setUniform("ourColor", Red, Green, Blue, 1.0f);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, sizeof(index) / sizeof(index[0]), GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ Lab_4-master/Lab1_Graph/vertex_shader.glsl
@@ -0,0 +1,9 @@
+#version 410 core
+layout(location = 0) in vec3 vp;
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+uniform mat4 transform;
+void main(){
+    gl_Position = projection*view*vec4(vp.x, vp.y, vp.z, 1.0);
+}
